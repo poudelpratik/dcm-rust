49633830ecbff795a761a88c5fdf43c5f7955ed4
Pratik Poudel <poudel@data-cybernetics.com>
2023-11-28 18:26:50 +0100

    remove comments in script

--- run.sh
+++ run.sh
@@ -2,8 +2,3 @@ sudo chmod -R 777 . &&
 sudo docker compose --profile generate-wasm up --build &&
 sudo chmod -R 777 . &&
 sudo docker compose --profile run-app up --build
-
-#sudo chmod -R 777 . &&
-#sudo docker compose up wasm-generator &&
-#sudo docker compose up code-distributor &&
-#sudo docker compose up runtime-code-mobility-demo
