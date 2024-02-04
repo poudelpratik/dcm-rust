dcafb16be1cd48165669f1ed8a754a20b6e28343
Pratik Poudel <poudel@data-cybernetics.com>
2024-02-04 22:23:13 +0100

    minor fix

--- web-application/runtime-code-mobility-demo/src/shared/playground2.rs
+++ web-application/runtime-code-mobility-demo/src/shared/playground2.rs
@@ -10,9 +10,10 @@
 //
 // /// @mobile
 // pub fn area_of_circle(radius: f64) -> f64 {
-//     const PI: f64 = std::f64::consts::PI;
 //     PI * square(radius)
 // }
+//
+// const PI: f64 = std::f64::consts::PI;
 //
 // /// @mobile(crates = ["quote"])
 // pub fn generate_struct_code() -> CustomString {
